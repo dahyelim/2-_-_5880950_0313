68e3bb51c3f216e8e80e8ec206fa55fd75f8c5eb
dahyelim <lim3016566@naver.com>
2025-03-14 23:35:18 +0900

    2과제

--- 2완성_임다혜_5880950_0313/2완성_임다혜_5880950_0313.cpp
+++ 2완성_임다혜_5880950_0313/2완성_임다혜_5880950_0313.cpp
@@ -1,34 +1,29 @@
 #include <stdio.h>
 #include <stdint.h>
-#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
 
 int main() {
     float num;
-    // 실수 입력 받기
     printf("실수를 입력하세요: ");
     scanf_s("%f", &num);
 
-    // float 값을 32비트로 취급하기 위해 union 사용
-    union {
-        float f;
-        uint32_t i;
-    } u;
+    // 32비트로 실수 값의 비트 패턴을 저장
+    uint32_t bits;
+    memcpy(&bits, &num, sizeof(bits));  // 실수를 32비트 정수로 변환
 
-    u.f = num;
+    // 부호, 지수, 가수 비트를 추출
+    int sign = (bits >> 31) & 0x1;  // 부호 비트
+    int exponent = (bits >> 23) & 0xFF;  // 지수 비트 (8비트)
+    int fraction = bits & 0x7FFFFF;  // 가수 비트 (23비트)
 
-    // 부호 
-    int sign = (u.i >> 31) & 0x1;
-
-    // 지수 
-    int exponent = ((u.i >> 23) & 0xFF) - 127;
-
-    // 가수
-    int mantissa = u.i & 0x7FFFFF;
-
-    // 부호, 지수, 가수 출력
-    printf("부호 : %d\n", sign);
-    printf("지수: %02X\n", exponent + 127);  // 바이어스를 추가하여 출력
-    printf("가수: %06X\n", mantissa);
+    // 부호, 지수, 가수 비트를 출력
+    printf("부호 비트: %d\n", sign);
+    printf("지수 비트: %08X\n", exponent);  // 8비트 16진수 출력
+    printf("가수 비트: ");
+    for (int i = 22; i >= 0; i--) {  // 23비트 가수 출력
+        printf("%d", (fraction >> i) & 0x1);
+    }
+    printf("\n");
 
     return 0;
 }
